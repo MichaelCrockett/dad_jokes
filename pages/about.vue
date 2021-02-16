a2b072eb45fe405de5d3137c380accc10941690c
Michael Crockett <mike.crockett76@gmail.com>
2021-02-16 11:52:21 +0000

    Initial setup and styling

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div class="">
+    <h1>About this DadJokes</h1>
+    <p>This is an app that displays corny dad jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'About The App',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
